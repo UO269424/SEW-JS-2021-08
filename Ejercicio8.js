4a945a78c388f328e4f54198fc291df56d507f35
Alonso Gago <60621183+UO269424@users.noreply.github.com>
2021-11-30 23:27:50 +0100

    Add files via upload

---
+++ Ejercicio8.js
@@ -0,0 +1,51 @@
+"use strict";
+class Meteo{
+    constructor(){
+        this.apikey = "3ea42e9a9b61d1376c4e216765791cc0";
+        this.unidades = "&units=metric";
+        this.idioma = "&lang=es";
+        this.error = "<h2>¡problemas! No puedo obtener información de <a href='https://openweathermap.org'>OpenWeatherMap</a></h2>";
+    }
+    
+    cargarDatos(ciudad, codigoPais){
+        var url = "https://api.openweathermap.org/data/2.5/weather?q=" + ciudad + "," + codigoPais + this.unidades + this.idioma + "&APPID=" + this.apikey;
+        
+        $.ajax({
+            dataType: "json",
+            url: url,
+            method: 'GET',
+            success: function(datos){
+                $('.json').text(JSON.stringify(datos, null, 2));
+
+                $('.datos').empty();    
+                $('.datos').append('<img src="https://openweathermap.org/img/w/' + datos.weather[0].icon + '.png" alt="img weather">');
+                $('.datos').append("<br>Ciudad: " + datos.name + "<br>");
+                $('.datos').append("País: " + datos.sys.country + "<br>");
+                $('.datos').append("Latitud: " + datos.coord.lat + " grados<br>");
+                $('.datos').append("Longitud: " + datos.coord.lon + " grados<br>");
+                $('.datos').append("Temperatura: " + datos.main.temp + " grados Celsius<br>");
+                $('.datos').append("Máx temperatura: " + datos.main.temp_max + " celsius<br>");
+                $('.datos').append("Min temperatura: " + datos.main.temp_min + " celsius<br>");
+                $('.datos').append("Presión: " + datos.main.pressure + " milímetros<br>");
+                $('.datos').append("Humedad: " + datos.main.humidity + "%<br>"); 
+                $('.datos').append("Amanece a las: " + new Date(datos.sys.sunrise *1000).toLocaleTimeString() + "<br>"); 
+                $('.datos').append("Oscurece a las: " + new Date(datos.sys.sunset *1000).toLocaleTimeString() + "<br>"); 
+                $('.datos').append("Velocidad del viento: " + datos.wind.speed + "metros/segundo<br>"); 
+                $('.datos').append("Dirección del viento: " + datos.wind.deg +"grados<br>");
+                $('.datos').append("Fecha de la medida: " + new Date(datos.dt *1000).toLocaleDateString() + "<br>");
+                $('.datos').append("Descripción: " + datos.weather[0].description + "<br>");
+                $('.datos').append("Visibilidad: " + datos.visibility + " metros<br>");
+                $('.datos').append("Nubosidad: " + datos.clouds.all + " %");
+            },
+            error:function(){
+                document.write(this.error);    
+            }
+        });
+    }
+
+    verJSON(ciudad, codigoPais){
+        this.cargarDatos(ciudad, codigoPais);
+    }
+}
+
+var meteo = new Meteo();
